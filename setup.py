bb019ea180b2897e037c2c4a853eec1b3b6fd45e
Zhehao Mao <zhehao.mao@gmail.com>
2011-12-24 10:01:11 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="funktown",
+      version="0.1",
+	  description="Functional Data Structures for Python",
+	  author="Zhehao Mao",
+	  author_email="zhehao.mao@gmail.com",
+	  url="http://github.com/zhemao/funktown",
+	  packages=["funktown"]
+	 )
