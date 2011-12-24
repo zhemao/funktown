dcfee824357fb7bbed41e6e25ec91dc4e2918910
Zhehao Mao <zhehao.mao@gmail.com>
2011-12-23 21:44:06 -0500

    use python's builtin hash function

--- funcds/dict.py
+++ funcds/dict.py
@@ -1,37 +1,30 @@
 from .lookuptree import LookupTree
 
-def hashfunc(key):
-	h = 2166136261
-	for c in key:
-		h = ( h * 16777619 ) ^ ord(c)
-	return h 
-	
-
 class ImmutableDict:
 
 	def __init__(self, initdict=None, **kwargs):
 		if initdict == None: initdict = {}
 		initdict.update(kwargs)
-		hashlist = [(hashfunc(key), (key, initdict[key])) for key in initdict]
+		hashlist = [(hash(key), (key, initdict[key])) for key in initdict]
 		self.tree = LookupTree(dict(hashlist))
 		self._length = len(initdict)
 
 	def assoc(self, key, value):
 		copydict = ImmutableDict()
-		copydict.tree = self.tree.assoc(hashfunc(key), (key, value))
+		copydict.tree = self.tree.assoc(hash(key), (key, value))
 		copydict._length = self._length + 1
 		return copydict
 
 	def extend(self, other):
 		copydict = ImmutableDict()
-		vallist = [(hashfunc(key), (key, other[key])) for key in other]
+		vallist = [(hash(key), (key, other[key])) for key in other]
 		copydict.tree = self.tree.multi_assoc(vallist)
 		copydict._length = iter_length(copydict.tree)
 		return copydict
 
 	def remove(self, key):
 		copydict = ImmutableDict()
-		copydict.tree = self.tree.remove(hashfunc(key))
+		copydict.tree = self.tree.remove(hash(key))
 		copydict._length = self._length - 1
 		return copydict
 
@@ -45,7 +38,7 @@ class ImmutableDict:
 
 	def __getitem__(self, key):
 		try:
-			return self.tree[hashfunc(key)][1]
+			return self.tree[hash(key)][1]
 		except KeyError: raise KeyError(key)
 
 	def __iter__(self):
