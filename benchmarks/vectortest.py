e99d0ddbfe1b25d2cc325c84c23c26d0d833fc8b
Zhehao Mao <zhehao.mao@gmail.com>
2011-12-25 10:24:55 -0500

    add benchmarks

---
+++ benchmarks/vectortest.py
@@ -0,0 +1,28 @@
+import moka
+import funktown
+import cProfile as profile
+
+def mokatest(fname):
+	mlist = moka.List()
+	with open(fname) as f:
+		for line in f:
+			mlist = mlist.append(line)
+
+def funktowntest(fname):
+	ftvec = funktown.ImmutableVector()
+	with open(fname) as f:
+		for line in f:
+			ftvec = ftvec.conj(line)
+
+def stdlibtest(fname):
+	lst = []
+	with open(fname) as f:
+		for line in f:
+			lst.append(line)
+
+if __name__ == "__main__":
+	fname = "studyinscarlet.txt"
+	profile.run('mokatest(fname)')
+	profile.run('funktowntest(fname)')
+	profile.run('stdlibtest(fname)')
+	
