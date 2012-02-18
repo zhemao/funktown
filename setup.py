3c4f8046ca7396c6de6ddc1363c9a556ac56a5dd
Zhehao Mao <zhehao.mao@gmail.com>
2012-02-18 11:04:35 -0500

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="funktown",
-      version="0.4.3",
+      version="0.4.4",
 	  description="Functional Data Structures for Python",
 	  author="Zhehao Mao",
 	  author_email="zhehao.mao@gmail.com",
